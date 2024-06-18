93a7bec91dd5185466a7f9f92257e2e1ccbd9ae8
tornikegomareli <gomarelidevelopment@gmail.com>
2024-06-18 23:58:20 +0400

    remove comments

--- yabai/scripts/open_kitty.sh
+++ yabai/scripts/open_kitty.sh
@@ -5,9 +5,7 @@ if ! pgrep -f "kitty" >/dev/null 2>&1; then
 	open -a "/Applications/kitty.app"
 else
 	# Send a signal to create a new window
-	# Note: Kitty does not support AppleScript; it relies on the kitty remote control feature
 	kitty @ new-window || {
-		# Get pids for any app with "kitty" and kill
 		while IFS="" read -r pid; do
 			kill -15 "${pid}"
 		done < <(pgrep -f "kitty")
