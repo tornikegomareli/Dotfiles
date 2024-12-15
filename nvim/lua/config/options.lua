0da9d961b1052e8e9636b74a2429e9d095012935
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2024-12-15 23:10:43 +0400

    font increased

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,4 +3,16 @@
 -- Add any additional options here
 --
 
-vim.opt.guifont = "Iosevka Term:h15"
+vim.opt.guifont = "Iosevka Term:h24"
+
+-- Brackets and some formatting
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "swift",
+  callback = function()
+    vim.opt_local.cindent = true
+    vim.opt_local.cinoptions = "L0"
+    vim.opt_local.shiftwidth = 2 -- Set indent to 2 spaces
+    vim.opt_local.tabstop = 2 -- Set tab to 2 spaces
+    vim.opt_local.softtabstop = 2 -- Set soft tab to 2 spaces
+  end,
+})
