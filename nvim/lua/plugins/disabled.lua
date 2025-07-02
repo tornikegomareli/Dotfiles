dc7288ff9ea6ed5ba5d76d5acc903a8fd1acc28e
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-07-02 21:44:33 +0400

    nvim update

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+    },
+  },
+}
